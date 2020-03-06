4a1a21576d7d15cd6fb07968bdc5c3207f028246
Trish Ta <trish.ta@shopify.com>
2020-03-05 21:10:18 -0500

    migrate redirect calls to App Bridge

--- app/assets/javascripts/shopify_app/redirect.js
+++ app/assets/javascripts/shopify_app/redirect.js
@@ -1,26 +1,21 @@
+//= require ./app_bridge_redirect.js
+
 (function() {
   function redirect() {
-    var redirectTargetElement = document.getElementById("redirection-target");
-
+    var redirectTargetElement = document.getElementById("redirection-target");  
     if (!redirectTargetElement) {
       return;
     }
-
+    
+    
     var targetInfo = JSON.parse(redirectTargetElement.dataset.target)
-
+    
     if (window.top == window.self) {
       // If the current window is the 'parent', change the URL by setting location.href
       window.top.location.href = targetInfo.url;
     } else {
-      // If the current window is the 'child', change the parent's URL with postMessage
-      normalizedLink = document.createElement('a');
-      normalizedLink.href = targetInfo.url;
-
-      data = JSON.stringify({
-        message: 'Shopify.API.remoteRedirect',
-        data: {location: normalizedLink.href}
-      });
-      window.parent.postMessage(data, targetInfo.myshopifyUrl);
+      // If the current window is the 'child', change the parent's URL with App Bridge Redirect
+      appBridgeRedirect(targetInfo.url);
     }
   }
 
--- app/assets/javascripts/shopify_app/storage_access.js
+++ app/assets/javascripts/shopify_app/storage_access.js
@@ -1,3 +1,4 @@
+//= require ./app_bridge_redirect.js
 (function() {
   var ACCESS_GRANTED_STATUS = 'storage_access_granted';
   var ACCESS_DENIED_STATUS = 'storage_access_denied';
@@ -11,17 +12,7 @@
   }
 
   StorageAccessHelper.prototype.redirectToAppTLD = function(storageAccessStatus) {
-    var normalizedLink = document.createElement('a');
-
-    normalizedLink.href = this.setNormalizedLink(storageAccessStatus);
-
-    data = JSON.stringify({
-      message: 'Shopify.API.remoteRedirect',
-      data: {
-        location: normalizedLink.href,
-      }
-    });
-    window.parent.postMessage(data, this.redirectData.myshopifyUrl);
+    appBridgeRedirect(this.setNormalizedLink(storageAccessStatus));
   }
 
   StorageAccessHelper.prototype.redirectToAppsIndex = function() {
